bb59d4238074ab4ef6b7b24f0a04fd02cf73257c
antonio-1983 <72706037+antonio-1983@users.noreply.github.com>
2022-11-14 20:19:18 -0300

    Update import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO producto (descripcion,imagen,precio)
+VALUES ('Sillon chenille','sillon chenille redondo - copia.jpg',7000),
+		('Silla capitone','silla gris.jpg',10000),	
+		('Butaca capitone','butacon panavioleta.jpg',8000),
+		('Banqueta redonda','redonda gris.jpg',7500),
+		('Sillon rayado','sillon chenille.jpg',7600),
+		('Respaldo capitone','respaldo1.jpg',15000),
+		('Banqueta romana','banqueta romana chenille.jpg',9000),
+		('Banqueta chaise','banqueta acqua.jpg',11000);
